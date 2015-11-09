cb656971d3541cbf768fee6dc4b6f3672cfd338a
pravegali <pravegali@gmail.com>
2015-11-08 20:15:40 -0500

    First Committ with Code
    A web api project created.

---
+++ Code/PoemGenerator/PoemGenerator.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PoemGenerator.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Code/PoemGenerator/PoemGenerator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PoemGenerator.WebApiApplication" Language="C#" %>
