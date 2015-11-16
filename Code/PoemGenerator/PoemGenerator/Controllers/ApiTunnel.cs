5fe00de70ede825c7de2fe6d2a3aa2c19e779d1d
pravegali <pravegali@gmail.com>
2015-11-16 03:57:42 -0500

    Final Committ, without tests

---
+++ Code/PoemGenerator/PoemGenerator/Controllers/ApiTunnel.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.IO;
+
+using PoemGenerator.Models;
+
+namespace PoemGenerator.Controllers
+{
+    
+    public class ApiTunnelController : ApiController
+    {
+        public async Task<HttpResponseMessage> PostFormData()
+    {
+        // Check if the request contains multipart/form-data.
+        if (!Request.Content.IsMimeMultipartContent())
+        {
+            throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+        }
+
+        string root = HttpContext.Current.Server.MapPath("~/App_Data");
+        var provider = new MultipartFormDataStreamProvider(root);
+
+        try
+        {
+            // Read the form data.
+            await Request.Content.ReadAsMultipartAsync(provider);
+
+            // This illustrates how to get the file names.
+            foreach (MultipartFileData file in provider.FileData)
+            {
+                    string[] FileText;
+                    string poem = "";
+                        if (File.Exists(file.LocalFileName))
+                        {
+                            FileText = File.ReadAllLines(file.LocalFileName);
+                        PoemCore poemObj = new PoemCore(FileText);
+                        poem=poemObj.GetPoem();
+
+                        var response = new HttpResponseMessage();
+                        response.Content = new StringContent("<html><body> <h2>"+poem+"</h2></body></html>");
+                        response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/html");
+                        return response;
+                    }
+                        else
+                        {
+                            throw new FileNotFoundException();
+                        }
+                    
+                    Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+                Trace.WriteLine("Server file path: " + file.LocalFileName);
+                    Trace.WriteLine(FileText[0]+FileText[1]);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+        catch (System.Exception e)
+        {
+            return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+        }
+    }
+
+
+
+    }
+}
---
+++ Code/PoemGenerator/PoemGenerator/Models/PoemCore.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PoemGenerator.Models
+{
+    public class PoemCore
+    {
+        Dictionary<String,List<List<String>>> cfg= new Dictionary<string, List<List<string>>>();
+        String inputFilePathe = "";
+        Random rnd = new Random();
+        string[] FileLines;
+        string poem;
+        public Dictionary<String, List<List<String>>> CFG
+        {
+            get
+            {
+                return cfg;
+            }
+            set
+            {
+                cfg = value;
+            }
+
+        }
+
+
+        public String InputFile
+        {
+            get
+            { return inputFilePathe; }
+
+            set
+            {
+                inputFilePathe = value;
+            }
+
+        }
+
+        public PoemCore(string[] FileLines)
+        {
+            this.FileLines=FileLines;
+
+            CFGDictionaryPrep(FileLines);
+        }
+
+        public void CFGDictionaryPrep(string[] FileLines)
+        {
+            string[] RuleValuePairs;
+            string[] LineSecondPart;
+            string[] RuleOrTerminals;
+            
+            List<string> values = new List<string>();
+            foreach (string FileLine in FileLines)
+            {
+                List<List<string>> cfgValue = new List<List<string>>();
+                RuleValuePairs = FileLine.Split(new string[] {": "}, StringSplitOptions.None);
+                LineSecondPart = RuleValuePairs[1].Split(' ');
+                
+                //values = new List<string>(LineSecondPart);
+                foreach (string Secondpart in LineSecondPart)
+                {
+                    RuleOrTerminals = Secondpart.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                    values = new List<string>(RuleOrTerminals);
+                    cfgValue.Add(values);
+                }
+
+                cfg.Add(RuleValuePairs[0], cfgValue);
+            }
+        }
+
+        public string GetPoem()
+        {
+            
+            int LineCount = 0;
+            List<List<string>> CFGValue=new List<List<string>>();
+           //Get the line count
+            if (cfg.TryGetValue("POEM", out CFGValue))
+            {
+                LineCount=CFGValue.Count;
+
+            }
+
+            //form each line
+            for (int i=1 ;i<= LineCount;i++ )
+            {
+
+                EvaluateComponent("LINE");
+
+            }
+
+            return this.poem;
+        }
+
+        public void EvaluateComponent(string word)
+        {
+            string keyword = "";
+            List<List<string>> CFGValue = new List<List<string>>();
+            if (cfg.TryGetValue(word, out CFGValue))
+            {
+                //choose random string from the first list of strings the CFGValue list. 
+                foreach (List<string> value in CFGValue)
+                {
+                    int r = rnd.Next(value.Count);
+                    //check the first char
+                    //if it is $ then it is a command
+                    char firstChar = value[r][0];
+                    if (value[r][0] == '$')
+                    {
+                        //execute the command;
+                        if (value[r].Split('$')[1] == "LINEBREAK")
+                        {
+                            poem += " "+"<br/>";
+
+                             
+                        }
+                        if (value[r].Split('$')[1] == "END")
+                        {
+
+                            poem += " " + "<br/>";
+                            return ;
+                        }
+
+
+                    }
+                    else if(value[r][0]=='<')
+                    {
+                        string ruleOrWordOrCmd = value[r].Split(new char[] { '<', '>' })[1];
+                        EvaluateComponent(ruleOrWordOrCmd);
+                    }
+
+                    else
+                    {
+                        poem +=" "+ value[r];
+                    }
+
+
+                }
+                
+            }
+            return;
+
+        }
+
+        
+    }
+}
